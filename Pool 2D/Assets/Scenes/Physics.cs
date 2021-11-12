6d376c34bf4af537139ee547fa670b9278a4eda4
ImLashe <lalleenicolaselian@gmail.com>
2021-11-12 11:01:38 -0300

    mas cambios al codigo xd

--- Pool 2D/Assets/Scenes/Physics.cs
+++ Pool 2D/Assets/Scenes/Physics.cs
@@ -5,23 +5,43 @@ using UnityEngine;
 
 public class Physics : MonoBehaviour
 {
-    float Speed = 1;
-    float Force = 5;
-    float Mass = 0.16f;
-
+    [SerializeField]float speed = 1;
+    [SerializeField]float force = 3;
+    [SerializeField]float mass = 1f;
+    [SerializeField]float gravity = 9.8f;
+    [SerializeField]float miu = 0.02f;
+    [SerializeField]float acceleration =0;
+    [SerializeField]float friction =0;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        float normalForce = (mass * gravity);
+        acceleration = (force / mass);
+        friction = (miu * normalForce);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        float Aceleration = (Mass / Force);
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            acceleration = (force / mass);
+            speed = 1;
+        }
 
-        transform.position +=Vector3.forward * (Speed + Aceleration * Time.deltaTime);
+        acceleration -= friction;     
+
+        speed += acceleration;
+
+        if (speed <= 0)
+        {
+            speed = 0;
+        }
+
+
+        transform.position +=Vector3.right * (speed * Time.deltaTime);
         
     }
 }
