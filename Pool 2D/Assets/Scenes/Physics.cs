71110f4f28a80a2142cb135626ebdfd5794b6f4d
ImLashe <lalleenicolaselian@gmail.com>
2021-11-12 09:23:59 -0300

    cambio codigo

--- Pool 2D/Assets/Scenes/Physics.cs
+++ Pool 2D/Assets/Scenes/Physics.cs
@@ -2,17 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Physics : MonoBehaviour
 {
+    float Speed = 1;
+    float Force = 5;
+    float Mass = 0.16f;
+
+
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position += new Vector3(1 * Time.deltaTime, 0, 0);
+        float Aceleration = (Mass / Force);
+
+        transform.position +=Vector3.forward * (Speed + Aceleration * Time.deltaTime);
+        
     }
 }
